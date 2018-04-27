c9cf24f8a9a7a73ed45a54484df3c2159cd4fa9f
Simon Dupree <dupreenator@gmail.com>
2018-04-27 08:46:49 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+var box = document.getElementById('display');
+
+//toScreen - user input gets shown in box aka display
+function toScreen(x) {
+  //+= to chain numbers next to each other x=x+x+....
+  box.value += x;
+  if (x === 'c') {
+    box.value = '';
+  }
+}
+
+function answer() {
+  x = box.value;
+  x = eval(x);
+  //evaluates strings as math eval(string)
+  box.value = x;
+}
+
+function power() {
+  x=box.value;
+  x=eval(x*x);
+  box.value = x;
+}
+
+function backspace() {
+  var num = box.value;
+  var len = num.length-1;
+  var newNum = num.substring(0,len);
+  box.value = newNum;
+}
